1014d33c98af2357dc4f96c89c2a7604770f152c
Ayush <ayushsinghlts123@gmail.com>
2025-05-22 00:57:40 +0530

    Trying one more logic

--- src/Practice/string/GCDStrings.java
+++ src/Practice/string/GCDStrings.java
@@ -10,6 +10,15 @@ public class GCDStrings {
         if(!str1.concat(str2).equals(str2.concat(str1))) {
             return "";
         }
+//        String s = str1.concat(str2);
+//        int j = 0;
+//        int length = 0;
+//        for (int i = 1; i < s.length(); i++) {
+//            if(s.charAt(j) == s.charAt(i)) {
+//                length = i;
+//                break;
+//            }
+//        }
         int length = gcd(str1.length(), str2.length());
         return str1.substring(0, length);
     }
@@ -17,8 +26,8 @@ public class GCDStrings {
     public static int gcd(int a, int b) {
         while(b != 0) {
             int temp = b;
-            b = a % b;
-            a = temp;
+            b = a % b;  //5
+            a = temp; //10
         }
         return a;
     }
